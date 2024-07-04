a016043827125b7ced320fbe0ced092a82400773
bmo <szymonpiechutowski@gmail.com>
2024-07-04 09:49:11 +0200

    Made properties and methods private

--- src/growbonsai/clock.py
+++ src/growbonsai/clock.py
@@ -29,36 +29,36 @@ class Clock(Static):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         # Widgets
-        self.h_min = Static('')
-        self.t_min = Static('')
-        self.u_min = Static(NUMBERS_DICT['0'])
-        self.t_sec = Static(NUMBERS_DICT['0'])
-        self.u_sec = Static(NUMBERS_DICT['0'])
-        self.seed_button = Button('Seed', variant='success', id='seed-bt')
+        self._h_min = Static('')
+        self._t_min = Static('')
+        self._u_min = Static(NUMBERS_DICT['0'])
+        self._t_sec = Static(NUMBERS_DICT['0'])
+        self._u_sec = Static(NUMBERS_DICT['0'])
+        self._seed_button = Button('Seed', variant='success', id='seed-bt')
 
         # Data
-        self.clock_mode: str = 'Timer'
-        self.stop_watch_started: datetime = datetime.now()
-        self.timer_length: int = 0
+        self._clock_mode: str = 'Timer'
+        self._stop_watch_started: datetime = datetime.now()
+        self._timer_length: int = 0
         self._cancel_timer_counter_default = 3
-        self.cancel_timer_counter: int = 0
+        self._cancel_timer_counter: int = 0
 
         # Intervals
-        self.intervals = []
+        self._intervals = []
 
     def compose(self):
         with Horizontal(id='clock-wrapper'):
-            yield self.h_min
+            yield self._h_min
             yield Label(' ')
-            yield self.t_min
+            yield self._t_min
             yield Label(' ')
-            yield self.u_min
+            yield self._u_min
             yield Label(' ')
             yield Static(NUMBERS_DICT[':'])
             yield Label(' ')
-            yield self.t_sec
+            yield self._t_sec
             yield Label(' ')
-            yield self.u_sec
+            yield self._u_sec
         yield Input(
             value='45',
             placeholder='Type number from 5 to 300',
@@ -66,91 +66,91 @@ class Clock(Static):
             validators=[ValueFrom5to300()],
             id='session-duration'
         )
-        yield self.seed_button
+        yield self._seed_button
 
     @on(Button.Pressed, '#seed-bt')
-    def start_timer(self) -> None:
+    def _start_timer(self) -> None:
         """Start timer or stopwatch depend on the chosen mode"""
-        if self.seed_button.variant == 'success':
-            self.start_seeding()
-        elif self.seed_button.variant == 'warning':
+        if self._seed_button.variant == 'success':
+            self._start_seeding()
+        elif self._seed_button.variant == 'warning':
             # Reset Timer
-            self.h_min.update('')
-            self.t_min.update('')
-            self.u_min.update(NUMBERS_DICT['0'])
-            self.t_sec.update(NUMBERS_DICT['0'])
-            self.u_sec.update(NUMBERS_DICT['0'])
+            self._h_min.update('')
+            self._t_min.update('')
+            self._u_min.update(NUMBERS_DICT['0'])
+            self._t_sec.update(NUMBERS_DICT['0'])
+            self._u_sec.update(NUMBERS_DICT['0'])
             # Reset Button
-            self.seed_button.variant = 'success'
-            self.seed_button.label = 'Seed'
+            self._seed_button.variant = 'success'
+            self._seed_button.label = 'Seed'
             # Stop intervals
-            self.stop_intervals()
+            self._stop_intervals()
         else:
             pass
 
-    def stop_intervals(self):
+    def _stop_intervals(self):
         """Stop all active intervals."""
-        for interval in self.intervals:
+        for interval in self._intervals:
             interval.stop()
-        self.intervals.clear()
+        self._intervals.clear()
 
-    def start_seeding(self) -> None:
+    def _start_seeding(self) -> None:
         """Start clock"""
-        self.cancel_timer_counter = self._cancel_timer_counter_default
-        if self.clock_mode == 'Timer':
-            self.timer_length = int(self.query_one(Input).value) * 60
-            interval1 = self.set_interval(1, self.timer)
+        self._cancel_timer_counter = self._cancel_timer_counter_default
+        if self._clock_mode == 'Timer':
+            self._timer_length = int(self.query_one(Input).value) * 60
+            interval1 = self.set_interval(1, self._timer)
         else:
-            self.stop_watch_started = datetime.now()  # Reload time
-            interval1 = self.set_interval(1, self.stopwatch)
+            self._stop_watch_started = datetime.now()  # Reload time
+            interval1 = self.set_interval(1, self._stopwatch)
 
-        self.seed_button.variant = 'warning'
-        interval2 = self.set_interval(1, self.start_cancel_timer, repeat=30)
-        self.intervals.extend((interval1, interval2))
+        self._seed_button.variant = 'warning'
+        interval2 = self.set_interval(1, self._start_cancel_timer, repeat=30)
+        self._intervals.extend((interval1, interval2))
 
-    def stopwatch(self) -> None:
+    def _stopwatch(self) -> None:
         """Update variable used by stopwatch and update displayed time"""
-        elapsed_time = datetime.now() - self.stop_watch_started
+        elapsed_time = datetime.now() - self._stop_watch_started
         minutes, seconds = divmod(elapsed_time.total_seconds(), 60)
-        self.update_clock(minutes, seconds)
+        self._update_clock(minutes, seconds)
 
-    def timer(self) -> None:
+    def _timer(self) -> None:
         """Update variable used by timer and update displayed time"""
-        self.timer_length -= 1
-        minutes, seconds = divmod(self.timer_length, 60)
-        self.update_clock(minutes, seconds)
+        self._timer_length -= 1
+        minutes, seconds = divmod(self._timer_length, 60)
+        self._update_clock(minutes, seconds)
 
-    def update_clock(self, minutes, seconds):
+    def _update_clock(self, minutes, seconds):
         """Update displayed digits"""
         minutes_str = str(int(minutes)).zfill(2)
         seconds_str = str(int(seconds)).zfill(2)
         print(f"{minutes_str}:{seconds_str}")
 
         if int(minutes) >= 100:
-            self.h_min.update(NUMBERS_DICT[minutes_str[-3]])
+            self._h_min.update(NUMBERS_DICT[minutes_str[-3]])
         else:
-            self.h_min.update('')
+            self._h_min.update('')
 
         if int(minutes) >= 10:
-            self.h_min.update(NUMBERS_DICT[minutes_str[-2]])
+            self._h_min.update(NUMBERS_DICT[minutes_str[-2]])
         else:
-            self.h_min.update('')
+            self._h_min.update('')
 
-        self.u_min.update(NUMBERS_DICT[minutes_str[-1]])
-        self.t_sec.update(NUMBERS_DICT[seconds_str[-2]])
-        self.u_sec.update(NUMBERS_DICT[seconds_str[-1]])
+        self._u_min.update(NUMBERS_DICT[minutes_str[-1]])
+        self._t_sec.update(NUMBERS_DICT[seconds_str[-2]])
+        self._u_sec.update(NUMBERS_DICT[seconds_str[-1]])
 
-    def start_cancel_timer(self):
+    def _start_cancel_timer(self):
         """Allow user to cancel timer in first 30 seconds"""
-        self.cancel_timer_counter -= 1
-        if self.cancel_timer_counter > 0:
-            self.seed_button.label = f'Cancel ({self.cancel_timer_counter})'
+        self._cancel_timer_counter -= 1
+        if self._cancel_timer_counter > 0:
+            self._seed_button.label = f'Cancel ({self._cancel_timer_counter})'
         else:
-            self.seed_button.label = 'Kill'
-            self.seed_button.variant = 'error'
+            self._seed_button.label = 'Kill'
+            self._seed_button.variant = 'error'
 
     @on(Input.Changed, '#session-duration')
-    def session_duration_changed(self, event: Input.Changed) -> None:
+    def _session_duration_changed(self, event: Input.Changed) -> None:
         """If the session duration is not correct block start button."""
         if event.input.is_valid:
             self.query_one('#seed-bt', Button).disabled = False
@@ -159,11 +159,11 @@ class Clock(Static):
 
     def change_clock_mode(self):
         """Change between `timer` and `stopwatch`"""
-        if self.clock_mode == 'Timer':
-            self.clock_mode = self.app.title = 'Stopwatch'
+        if self._clock_mode == 'Timer':
+            self._clock_mode = self.app.title = 'Stopwatch'
             self.query_one(Input).remove()
         else:
-            self.clock_mode = self.app.title = 'Timer'
+            self._clock_mode = self.app.title = 'Timer'
             self.mount(
                 Input(
                     value='45',
