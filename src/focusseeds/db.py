2175d81df26bfa4947b7cca677be2aa06fef9bce
bmo <szymonpiechutowski@gmail.com>
2024-07-15 17:12:58 +0200

    Removed function that was causing db error

--- src/focusseeds/db.py
+++ src/focusseeds/db.py
@@ -1,5 +1,5 @@
 import sqlite3
-from sqlite3 import Connection
+from sqlite3 import Connection, connect
 from pathlib import Path
 from datetime import datetime
 
@@ -10,13 +10,9 @@ class DatabaseManager:
     def __init__(self):
         self.db_file = Path(user_data_dir()) / 'Focus-Seeds/user_data/focus_seeds.db'
 
-    def connect(self) -> Connection:
-        """Return DB connection"""
-        return sqlite3.connect(self.db_file)
-
     def db_setup(self) -> None:
         """Method used only to set up DB on app initialization"""
-        with self.connect() as con:
+        with connect(self.db_file) as con:
             con.cursor().execute("""
                 CREATE TABLE study_sessions(
                 id INTEGER PRIMARY KEY,
@@ -28,7 +24,7 @@ class DatabaseManager:
             con.commit()
 
     def create_session_entry(self, length: int, is_successful: int):
-        with self.connect() as con:
+        with connect(self.db_file) as con:
             con.cursor().execute("""
                 INSERT INTO study_sessions(length, date, done)
                 VALUES (?, ?, ?)
