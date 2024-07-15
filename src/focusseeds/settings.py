e55a4a1797eb10d81412d8ebd4b3628afebf50f4
bmo <szymonpiechutowski@gmail.com>
2024-07-15 16:43:37 +0200

    Created placeholders for settings

--- src/focusseeds/settings.py
+++ src/focusseeds/settings.py
@@ -1,11 +1,28 @@
 from textual.app import ComposeResult
-from textual.widgets import Button, Footer
+from textual.widgets import Button, Footer, Static
 from textual.screen import Screen
+from textual.containers import Center, ScrollableContainer, Container, HorizontalScroll, VerticalScroll, Horizontal
 
 from focusseeds._app import AppHeader
 
 
 class SettingsScreen(Screen):
+    DEFAULT_CSS = """
+    #settings-wrapper {
+        align: center top; 
+    }
+    
+    #settings-body {
+        margin: 2 0;
+        width: 70;
+        height: auto;
+    }
+    
+    .settings-section {
+        border: round white;
+        height: auto;
+    }
+    """
     BINDINGS = [
         ('ctrl+q', 'quit_app', 'Quit App'),
         ('escape', 'close_settings', 'Close Settings'),
@@ -18,8 +35,41 @@ class SettingsScreen(Screen):
         """Method has to return anything to run callback"""
         self.dismiss(None)
 
+    def __init__(self):
+        super().__init__()
+        self.account_settings = Static(classes='settings-section')
+        self.account_settings.border_title = 'Account'
+
+        self.social_settings = Static(classes='settings-section')
+        self.social_settings.border_title = 'Social'
+
+        self.sound_settings = Static(classes='settings-section')
+        self.sound_settings.border_title = 'Sound'
+
+        self.theme_settings = Static(classes='settings-section')
+        self.theme_settings.border_title = 'Theme'
+
+        self.theme_store_settings = Static(classes='settings-section')
+        self.theme_store_settings.border_title = 'Theme Store'
+
+        self.about = Static(classes='settings-section')
+        self.about.border_title = 'About'
+
     def compose(self) -> ComposeResult:
         self.app.title = 'Settings'
         yield AppHeader()
-        yield Button(variant='success')
+        with (VerticalScroll(id='settings-wrapper')):
+            with Container(id='settings-body'):
+                with self.account_settings:
+                    yield Button('PLACEHOLDER')
+                with self.social_settings:
+                    yield Button('PLACEHOLDER')
+                with self.sound_settings:
+                    yield Button('PLACEHOLDER')
+                with self.theme_settings:
+                    yield Button('PLACEHOLDER')
+                with self.theme_store_settings:
+                    yield Button('PLACEHOLDER')
+                with self.about:
+                    yield Button('PLACEHOLDER')
         yield Footer()
