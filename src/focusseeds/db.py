e94c4cd85cf133b0b4a7416d5306864fa02c3ee3
bmo <szymonpiechutowski@gmail.com>
2024-07-16 14:07:24 +0200

    Change sound_settings table a little and add new method get_sound_name

--- src/focusseeds/db.py
+++ src/focusseeds/db.py
@@ -2,6 +2,7 @@ import sqlite3
 from sqlite3 import connect
 from pathlib import Path
 from datetime import datetime
+from typing import Literal
 
 from platformdirs import user_data_dir
 
@@ -24,20 +25,21 @@ class DatabaseManager:
             """)
             # Setup Settings table
             con.cursor().execute("""
-                CREATE TABLE settings_table(
+                CREATE TABLE sound_settings(
                     id INTEGER PRIMARY KEY,
-                    key TEXT,
-                    value TEXT
+                    type TEXT,
+                    name TEXT,
+                    is_default BIT
                 )
             """)
             con.commit()
             default_values = (
-                ('selected_alarm', 'Unfa_Braam'),
-                ('selected_signal', 'Unfa_Braam'),
-                ('selected_ambient', 'Unfa_Acid_Bassline')
+                ('alarm', 'Unfa_Braam.mp3', 1),
+                ('signal', 'Unfa_Braam.mp3', 1),
+                ('ambient', 'Unfa_Acid_Bassline.mp3', 1)
             )
             con.cursor().executemany(
-                "INSERT INTO settings_table (key, value) VALUES(?, ?)",
+                "INSERT INTO sound_settings(type, name, is_default) VALUES(?, ?, ?)",
                 default_values
             )
             con.commit()
@@ -49,3 +51,19 @@ class DatabaseManager:
                 VALUES (?, ?, ?)
             """, (length, datetime.now(), is_successful)
             )
+
+    def get_sound_name(
+            self,
+            sound_type: Literal['alarm', 'signal', 'ambient']
+    ) -> tuple[str, int]:
+        """Get from db file name and location of chosen sound
+        if int is 0 it is default else users sound
+        """
+        with connect(self.db_file) as con:
+            cur = con.cursor().execute(f"""
+                SELECT name, is_default FROM sound_settings WHERE type = '{sound_type}'
+            """)
+            return cur.fetchone()
+
+
+
