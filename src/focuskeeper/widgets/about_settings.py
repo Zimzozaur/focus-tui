ea588b2ca22448d67a4298e306a44fa72dd2300a
bmo <szymonpiechutowski@gmail.com>
2024-07-19 17:00:23 +0200

    Added more info to about

--- src/focuskeeper/widgets/about_settings.py
+++ src/focuskeeper/widgets/about_settings.py
@@ -2,24 +2,33 @@ import webbrowser
 
 from textual import on
 from textual.app import ComposeResult
-from textual.widgets import Button
-from textual.containers import Grid
+from textual.widgets import Button, Static
+from textual.containers import Grid, Container
 
 
-class AboutSettings(Grid):
+class AboutSettings(Container):
     DEFAULT_CSS = """
     AboutSettings {
+        height: auto;
+        padding: 1;    
+    }
+
+    #get-into-social {
         grid-size: 3 1;
         grid-gutter: 10;
         height: auto;
-        padding: 1;
+        padding-top: 1;
     }    
     """
 
     def compose(self) -> ComposeResult:
-        yield Button('Discord', id='discord')
-        yield Button('Github', id='github')
-        yield Button('X', id='x')
+        yield Static('Focus Keeper is your best buddy for working or studying.')
+        yield Static('If you want to learn more, share your ideas, or report bugs...')
+        yield Static('Check out our media!')
+        with Grid(id='get-into-social'):
+            yield Button('Discord', id='discord')
+            yield Button('Github', id='github')
+            yield Button('X', id='x')
 
     @on(Button.Pressed, '#discord')
     def discord_pressed(self) -> None:
