9db5b569a61979afd266573361a96c17ced0be04
bmo <szymonpiechutowski@gmail.com>
2024-07-20 17:19:04 +0200

    Updated

--- src/focuskeeper/modals/edit_sound.py
+++ src/focuskeeper/modals/edit_sound.py
@@ -1,4 +1,3 @@
-from pathlib import Path
 from typing import Literal
 
 from textual import on
@@ -11,10 +10,17 @@ from textual.widgets import Button, Collapsible, Input, Static
 from focuskeeper.config import AppConfig
 from focuskeeper.modals import AddSoundTree, ConfirmPopup
 from focuskeeper.widgets import Accordion
-from focuskeeper.utils.sound import sounds_list, remove_id_suffix, rename_file
+from focuskeeper.sound_manager import SoundManager
+
+
+def remove_id_suffix(string: str) -> str:
+    """Remove _something from the end of the string"""
+    return string[:string.rindex('_')]
 
 
 class EditSound(ModalScreen):
+    """EditSound allow user to perform CUD operation on sounds"""
+
     DEFAULT_CSS = """
     EditSound {
         align: center middle;
@@ -64,19 +70,19 @@ class EditSound(ModalScreen):
 
     def __init__(
             self,
-            sound_type: Literal['alarm', 'ambient'],
-            path_to_sounds: Path,
+            sound_type: Literal['short', 'long'],
             *args, **kwargs
     ) -> None:
         super().__init__(*args, **kwargs)
+        # Imported classes
         self.config = AppConfig()
+        self.sm = SoundManager()
+        # Type of sound, either 'alarm' or 'ambient'
         self.sound_type = sound_type
-        self.path_to_sounds = path_to_sounds
-        self.sounds_names: dict[str, str] = {
-            sound.split('.')[0]: f'.{sound.split('.')[1]}'
-            for sound in sounds_list(path_to_sounds)
-        }
-
+        if self.sound_type == 'short':
+            self.sounds_names = self.sm.user_shorts_list
+        else:
+            self.sounds_names = self.sm.user_longs_list
 
     def action_close_popup(self):
         self.dismiss(True)
@@ -93,7 +99,7 @@ class EditSound(ModalScreen):
     def compose(self) -> ComposeResult:
         with VerticalScroll(id='edit-sound-body'):
             with Accordion(id='sounds-accordion'):
-                for name in self.sounds_names.keys():
+                for name in self.sounds_names:
                     with Collapsible(title=name, classes='sound-collapsible', id=f'{name}_coll'):
                         yield Input(value=name, id=f"{name}_input", restrict=r'^[a-zA-Z0-9_-]+$')
                         with Horizontal(classes='sound-buttons-wrapper'):
@@ -106,7 +112,7 @@ class EditSound(ModalScreen):
             yield Static(id='add-sound-divider')
             with Center(id='add-sound-wrapper'):
                 yield Button(
-                    f"Add {'Sound' if self.sound_type != 'ambient' else 'Ambient'}",
+                    f"Add {'Sound' if self.sound_type != 'long' else 'Ambient'}",
                     variant='primary', id='add-sound-bt'
                 )
 
@@ -115,67 +121,51 @@ class EditSound(ModalScreen):
         """Check is new sound name correct"""
         query = f"#{remove_id_suffix(event.input.id)}_rename"
         sound_name = event.input.value
-        self.query_one(query).disabled = (sound_name in self.sounds_names or
-                                          sound_name in self.config.forbiden_sound_names())
+        self.query_one(query).disabled = sound_name in self.sm.all_shorts_longs_dict
 
     @on(Button.Pressed, '.sound-rename-bt')
     async def change_sound_name(self, event: Button.Pressed):
-        """Change name of a sound and update DOM and dist"""
+        """Change name of a sound and update DOM"""
         # Change name
-        sound_name = remove_id_suffix(event.button.id)
-        extension = self.sounds_names[sound_name]
-        old_name = sound_name + extension
-        new_name = self.query_one(f'#{sound_name}_input', Input).value
-        new_name_with_extension = new_name + extension
-        rename_file(self.path_to_sounds, old_name, new_name_with_extension)
-        # Update DOM and dict
-        del self.sounds_names[sound_name]
-        self.sounds_names[new_name] = extension
-        self.config.change_sound_name_if_in_config(self.sound_type, old_name, new_name_with_extension)
-        await self.recompose()
-        if self.sound_type == 'ambient':
+        old_name = remove_id_suffix(event.button.id)
+        new_name = self.query_one(f'#{old_name}_input', Input).value
+        self.sm.rename_sound(old_name, new_name)
+        # Update DOM
+        await self.recompose_(None)
+        if self.sound_type == 'long':
             self.notify('Changed name of an ambient')
         else:
             self.notify('Changed name of a sound')
+
         self.query_one(f'#{new_name}_coll', Collapsible).collapsed = False
 
     @on(Button.Pressed, '.sound-remove-bt')
     async def should_remove_sound(self, event: Button.Pressed):
-        """Display confirmation screen
-        if users accepts sound is removed from library
+        """Display confirmation screen if users accepts
+        Sound is removed from drive
         """
         async def remove_sound(boolean: bool) -> None:
             """Remove sound"""
             if not boolean:
                 return None
             # if removed sound that is already used
-            if self.config.is_sound_in_config(self.sound_type, sound_to_remove):
-                if self.sound_type == 'ambient':
-                    self.config.change_sound_to_default(self.sound_type, sound_to_remove)
-                else:
-                    self.config.change_sound_to_default(self.sound_type, sound_to_remove)
-
-            # Remove sound
+            self.sm.remove_sound(sound_name, self.sound_type)
             self.notify('Sound has been remove')
-            (self.path_to_sounds / sound_to_remove).unlink()
             await self.recompose_(None)
 
         sound_name = remove_id_suffix(event.button.id)
-        sound_to_remove = sound_name + self.sounds_names[sound_name]
-
         message = 'Are you sure you want to remove sound?'
         await self.app.push_screen(ConfirmPopup(message=message), remove_sound)
 
-
-    async def recompose_(self, arg):
-        """Refresh and recompose screen"""
-        self.sounds_names: dict[str, str] = {
-            sound.split('.')[0]: f'.{sound.split('.')[1]}'
-            for sound in sounds_list(self.path_to_sounds)
-        }
-        await self.recompose()
-
     @on(Button.Pressed, '#add-sound-bt')
-    def open_music_directory_tree(self):
-        self.app.push_screen(AddSoundTree(self.sound_type), self.recompose_)
+    async def open_music_directory_tree(self):
+        """Push AddSoundTree that allow user to add new songs"""
+        await self.app.push_screen(AddSoundTree(self.sound_type), self.recompose_)
 
+    async def recompose_(self, arg_from_callback) -> None:
+        """Update list before recompose"""
+        if self.sound_type == 'short':
+            self.sounds_names = self.sm.user_shorts_list
+        else:
+            self.sounds_names = self.sm.user_longs_list
+        await self.recompose()
