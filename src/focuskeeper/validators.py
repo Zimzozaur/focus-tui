0cf05d707be012977267bdd9fcb81f40157646ce
bmo <szymonpiechutowski@gmail.com>
2024-07-23 13:33:20 +0200

    Update of validator for dev reason

--- src/focuskeeper/validators.py
+++ src/focuskeeper/validators.py
@@ -1,8 +1,10 @@
 from textual.validation import Validator, ValidationResult
 
+from focuskeeper.settings import MIN_SESSION_LEN
+
 
 class ValueFrom5to300(Validator):
     def validate(self, value: str) -> ValidationResult:
-        if not value or int(value) < 5 or int(value) > 300:
-            return self.failure('Title cannot be longer than 50 chars')
+        if not value or int(value) < MIN_SESSION_LEN or int(value) > 300:
+            return self.failure('Session has to be between 5 and 300 minutes')
         return self.success()
