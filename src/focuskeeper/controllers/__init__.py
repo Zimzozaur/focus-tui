1990a2a1d23270f6daebf6af9cbaa59fb914f7f8
Simon <szymonpiechutowski@gmail.com>
2024-08-26 14:54:28 +0200

    Removed package

--- src/focuskeeper/controllers/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Collection of Controllers."""
-
-# Local imports.
-from .stopwatch import StopwatchController
-from .timer import TimerController
-
-# Public symbols
-__all__ = ["TimerController", "StopwatchController"]
