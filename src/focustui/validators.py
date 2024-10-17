2feb7d9a4feecea2b98581f0f9d140eeac008247
Simon <szymonpiechutowski@gmail.com>
2024-10-17 13:58:56 +0200

    Validator validates correctly

--- src/focustui/validators.py
+++ src/focustui/validators.py
@@ -5,13 +5,16 @@ from focustui.constants import MAX_SESSION_LEN as MAX_S_LEN
 from focustui.constants import MAX_VOLUME_LEVEL as MAX_V_LEV
 from focustui.constants import MIN_SESSION_LEN as MIN_S_LEN
 from focustui.constants import MIN_VOLUME_LEVEL as MIN_V_LEV
+from focustui.widgets import hour_min_session_len
+
+FORBIDDEN_PATTER: tuple = ("0:00", "0:0")
 
 
 class StopwatchOrTimer(Validator):
     def validate(self, value: str) -> ValidationResult:
         if (
-            value.isdigit() and
-            (int(value) == 0 or MIN_S_LEN <= int(value) <= MAX_S_LEN)
+                value.isdigit() and
+                (int(value) == 0 or MIN_S_LEN <= int(value) <= MAX_S_LEN)
         ):
             return self.success()
         return self.failure()
@@ -20,8 +23,12 @@ class StopwatchOrTimer(Validator):
 class StopwatchOrTimerHour(Validator):
     def validate(self, value: str) -> ValidationResult:
         pattern_bool = bool(HOURS_MINUTES_TIMER_PATTERN.match(value))
-        forbidden_patter: tuple = ("0:00", "0:0")
-        if pattern_bool and value not in forbidden_patter:
+        if (
+            pattern_bool and
+            value not in FORBIDDEN_PATTER and
+            (hour_min_session_len(value) == 0 or
+             (MIN_S_LEN <= hour_min_session_len(value) <= MAX_S_LEN))
+        ):
             return self.success()
         return self.failure()
 
