3368234a19742a19672495a97f94ae28f8b3ee2b
Simon <szymonpiechutowski@gmail.com>
2024-10-17 16:44:54 +0200

    Created utils to avoid curricular imports

---
+++ src/focustui/utils/__init__.py
@@ -0,0 +1,10 @@
+"""Collection of custom widget."""
+
+# Local imports.
+from .inputs import hour_min_session_len, int_to_hour_min
+
+# Public symbols
+__all__ = [
+    "hour_min_session_len",
+    "int_to_hour_min",
+]
---
+++ src/focustui/utils/inputs.py
@@ -0,0 +1,17 @@
+from focustui.constants import MINUTE
+
+
+def hour_min_session_len(value: str):
+    """Convert a time string in the format 'H:MM' to total minutes."""
+    length = value.split(":")
+    if len(length) == 1:
+        return 0
+    return int(length[0]) * MINUTE + int(length[1])
+
+
+def int_to_hour_min(value: int) -> str:
+    """Convert an int representing total minutes into a string formatted as 'H:MM'."""
+    if value == 0:
+        return "0"
+    hour, minute = divmod(value, MINUTE)
+    return f"{hour}:{minute:02d}"
