c7c82cb905fd740c82895f495eae47ffe348d643
Simon <szymonpiechutowski@gmail.com>
2024-10-25 21:25:43 +0200

    Removed unused widget.

--- src/focustui/widgets/__init__.py
+++ src/focustui/widgets/__init__.py
@@ -2,13 +2,11 @@
 
 # Local imports.
 from .accordion import Accordion
-from .app_header import AppHeader
 from .inputs import HourMinInput, MinInput, SoundVolumeInput
 from .music_directory_tree import MusicDirectoryTree
 
 # Public symbols
 __all__ = [
-    "AppHeader",
     "Accordion",
     "MusicDirectoryTree",
     "SoundVolumeInput",
--- src/focustui/widgets/app_header.py
+++
@@ -1,7 +0,0 @@
-from textual.widgets import Header
-from textual.widgets._header import HeaderTitle
-
-
-class AppHeader(Header):
-    def compose(self):
-        yield HeaderTitle()
