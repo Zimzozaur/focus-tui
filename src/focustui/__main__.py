12625bee45ac712a4c0a700a172c48212f2c096a
Simon <szymonpiechutowski@gmail.com>
2024-11-21 12:27:28 +0100

    Added CLI with path location and removed Pygame support prompt

--- src/focustui/__main__.py
+++ src/focustui/__main__.py
@@ -1,19 +1,53 @@
+from pathlib import Path
+
+import click
+from click import Choice, echo, style
+
 from focustui.app import FocusTUI
 from focustui.config_manager import ConfigManager
+from focustui.constants import (
+    CONFIG_FILE_PATH,
+    DB_FILE_PATH,
+    LONGS_PATH,
+    QUEUES_PATH,
+    SHORTS_PATH,
+    THEMES_PATH,
+)
 from focustui.db import DatabaseManager
 from focustui.setup import setup_app
 from focustui.sound_manager import SoundManager
 
+_paths = {
+    "db": DB_FILE_PATH,
+    "config": CONFIG_FILE_PATH,
+    "themes": THEMES_PATH,
+    "queues": QUEUES_PATH,
+    "shorts": SHORTS_PATH,
+    "longs": LONGS_PATH,
+}
 
-def main() -> None:
+
+@click.group(invoke_without_command=True)
+@click.pass_context
+def main(ctx) -> None:
     """Start app."""
-    setup_app()
-    FocusTUI(
-        db=DatabaseManager(),
-        cm=ConfigManager(),
-        sm=SoundManager(),
-    ).run()
+    if ctx.invoked_subcommand is None:
+        # Prevent app start on command use
+        setup_app()
+        FocusTUI(
+            db=DatabaseManager(),
+            cm=ConfigManager(),
+            sm=SoundManager(),
+        ).run()
 
 
-if __name__ == "__main__":
-    main()
+def echo_path(path: Path) -> None:
+    """Print given resource path."""
+    echo(style("Path: ", "green") + str(path))
+
+
+@main.command()
+@click.argument("what", type=Choice(list(_paths.keys())))
+def locate(what: str) -> None:
+    """Help you find location of a needed resource used by the app."""
+    echo_path(_paths[what])
